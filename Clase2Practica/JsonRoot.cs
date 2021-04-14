d7dae2bfc980c3b375a7b431f7a3bc15950fa59e
Pablo Sanchez <psanchez@plus54.com>
2021-04-14 20:54:12 -0300

    leyendo archivo json y conviriendolo a un objecto

---
+++ Clase2Practica/JsonRoot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2Practica
+{
+    public class JsonRoot
+    {
+        public List<Orden> ordenes { get; set; }
+    }
+    public class Orden
+    {
+        public int nroOrden { get; set; }
+        public string descripcion { get; set; }
+        public int cantidad { get; set; }
+        public int precio { get; set; }
+        public bool entregada { get; set; }
+    }
+}
--- Clase2Practica/Program.cs
+++ Clase2Practica/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace Clase2Practica
 {
@@ -6,14 +8,18 @@ namespace Clase2Practica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             //buscar archivo json
+            //Console.WriteLine("El directorio actual es:");
+            //string directorioDelJson = $@"..\.\..\..\{Directory.GetCurrentDirectory()}";
+
+            //para archivos grandes, se debe leer de a bloques utilizando un while
+            string jsonText = File.ReadAllText(@"..\.\..\..\..\ordenes.json");
 
             //parsear json
+            JsonRoot myDetails = JsonConvert.DeserializeObject<JsonRoot> (jsonText);
 
             //filtrar ordenes sin entregar
-
+            
             //mostrar por pantalla
 
 
