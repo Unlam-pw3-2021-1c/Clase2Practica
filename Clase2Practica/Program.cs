b5ce80828fc74970f05d45a1b9fbd4f8e290b4e2
Pablo Sanchez <psanchez@plus54.com>
2021-04-14 20:24:51 -0300

    commit inicial

---
+++ Clase2Practica/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Clase2Practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //buscar archivo json
+
+            //parsear json
+
+            //filtrar ordenes sin entregar
+
+            //mostrar por pantalla
+
+
+        }
+
+
+    }
+}
