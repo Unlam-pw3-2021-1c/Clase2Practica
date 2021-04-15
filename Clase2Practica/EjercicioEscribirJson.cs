4708b90f72cf018b8d6993f7ab0b08f686f0444c
Pablo Sanchez <psanchez@plus54.com>
2021-04-14 21:09:27 -0300

    agregando Escribir Json como TODO

---
+++ Clase2Practica/EjercicioEscribirJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase2Practica
+{
+    class EjercicioEscribirJson
+    {
+        public static void Ejecutar()
+        {
+            //TODO: Tarea armar ejemplo escribiendo json en un archivo
+        }
+    }
+}
