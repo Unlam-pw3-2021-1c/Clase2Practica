364e6ddd15443c1ea7e46011bb6065c8f840f0d5
Pablo Sanchez <psanchez@plus54.com>
2021-04-14 21:06:58 -0300

    refactorizando

---
+++ Clase2Practica/EjercicioLeerJson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Clase2Practica
+{
+    class EjercicioLeerJson
+    {
+        public static void Ejecutar()
+        {
+            //buscar archivo json
+            //Console.WriteLine("El directorio actual es:");
+            //string directorioDelJson = $@"..\.\..\..\{Directory.GetCurrentDirectory()}";
+
+            //para archivos grandes, se debe leer de a bloques utilizando un while
+            string jsonText = File.ReadAllText(@"..\.\..\..\..\ordenes.json");
+
+            //parsear json
+            JsonRoot jsonObj = JsonConvert.DeserializeObject<JsonRoot>(jsonText);
+
+            //filtrar ordenes sin entregar
+            List<Orden> ordenesSinEntregar = jsonObj.ordenes.Where(o => !o.entregada).ToList();
+
+            //mostrar por pantalla
+            Console.WriteLine("Las ordenes sin entregar son:");
+            Console.WriteLine("nroOrden, descripcion, cantidad, precio");
+            foreach (Orden orden in ordenesSinEntregar)
+            {
+                Console.WriteLine($"{orden.nroOrden}, {orden.descripcion}, {orden.cantidad}, {orden.precio}");
+            }
+        }
+    }
+}
--- Clase2Practica/Program.cs
+++ Clase2Practica/Program.cs
@@ -10,27 +10,7 @@ namespace Clase2Practica
     {
         static void Main(string[] args)
         {
-            //buscar archivo json
-            //Console.WriteLine("El directorio actual es:");
-            //string directorioDelJson = $@"..\.\..\..\{Directory.GetCurrentDirectory()}";
-
-            //para archivos grandes, se debe leer de a bloques utilizando un while
-            string jsonText = File.ReadAllText(@"..\.\..\..\..\ordenes.json");
-
-            //parsear json
-            JsonRoot jsonObj = JsonConvert.DeserializeObject<JsonRoot> (jsonText);
-
-            //filtrar ordenes sin entregar
-            List<Orden> ordenesSinEntregar = jsonObj.ordenes.Where(o => !o.entregada).ToList();
-
-            //mostrar por pantalla
-            Console.WriteLine("Las ordenes sin entregar son:");
-            Console.WriteLine("nroOrden, descripcion, cantidad, precio");
-            foreach (Orden orden in ordenesSinEntregar)
-            {
-                Console.WriteLine($"{orden.nroOrden}, {orden.descripcion}, {orden.cantidad}, {orden.precio}");
-            }
-
+            EjercicioLeerJson.Ejecutar();
         }
 
 
